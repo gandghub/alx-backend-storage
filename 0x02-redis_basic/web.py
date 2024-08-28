557637aea7586832bd6b130367ba90e49503749f
gandghub <hackthehacker33@gmail.com>
2024-08-28 14:27:37 +0000

    0x02-redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python3
+"""
+Caching request module
+"""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-# Initialize the Redis client
-rc = redis.Redis()
 
-
-def cache_page(method: Callable) -> Callable:
-    @wraps(method)
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
     def wrapper(url: str) -> str:
-        rc.incr(f"count:{url}")
-        cached_content = rc.get(f"cached:{url}")
-        if cached_content:
-            return cached_content.decode("utf-8")
-
-        try:
-            result = method(url)
-            rc.setex(f"cached:{url}", 10, result)
-            return result
-        except requests.exceptions.RequestException as e:
-            print(f"Error fetching the URL: {e}")
-            return "Error: Unable to fetch the URL."
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
     return wrapper
 
 
-@cache_page
+@track_get_page
 def get_page(url: str) -> str:
+    """ Makes a http request to a given endpoint
+    """
     response = requests.get(url)
     return response.text
-
-
-if __name__ == "__main__":
-    url = 'https://httpbin.org/delay/5'  # Replace with a reliable test URL
-    content = get_page(url)
-    print(content)
-    content = get_page(url)
-    print(content)
-    access_count = rc.get(f"count:{url}")
-    print(f"URL accessed {access_count.decode('utf-8')} times.")
